41a992a41807f305d090bba7cb6274ef852ae106
Not Tofu Food (Aiden Thakurdial) <53190357+NotTofuFood@users.noreply.github.com>
2020-08-24 22:20:57 -0500

    Update CompareImage.java

--- JavaFaceRecognition/src/FaceRegenition/CompareImage.java
+++ JavaFaceRecognition/src/FaceRegenition/CompareImage.java
@@ -27,58 +27,56 @@ public class CompareImage {
     		{ 
         		System.out.println(e); 
     		} 
-    int width1 = imgA.getWidth(); 
+		
+    		int width1 = imgA.getWidth(); 
 
-    int height1 = imgA.getHeight(); 
+    		int height1 = imgA.getHeight(); 
 
-    //Compare All The Pixels
+    		//Compare All The Pixels
 
-        long difference = 0; 
-        for (int y = 0; y < height1; y++) 
-        { 
-            for (int x = 0; x < width1; x++) 
-            { 
-                int rgbA = imgA.getRGB(x, y); 
-                int rgbB = imgB.getRGB(x, y); 
-                int redA = (rgbA >> 16) & 0xff; 
-                int greenA = (rgbA >> 8) & 0xff; 
-                int blueA = (rgbA) & 0xff; 
-                int redB = (rgbB >> 16) & 0xff; 
-                int greenB = (rgbB >> 8) & 0xff; 
-                int blueB = (rgbB) & 0xff; 
-                difference += Math.abs(redA - redB); 
-                difference += Math.abs(greenA - greenB); 
-                difference += Math.abs(blueA - blueB); 
-            } 
-        } 
+       		long difference = 0; 
+        	for (int y = 0; y < height1; y++) 
+        	{ 
+            		for (int x = 0; x < width1; x++) 
+            		{ 
+                		int rgbA = imgA.getRGB(x, y); 
+                		int rgbB = imgB.getRGB(x, y); 
+                		int redA = (rgbA >> 16) & 0xff; 
+               			int greenA = (rgbA >> 8) & 0xff; 
+                		int blueA = (rgbA) & 0xff; 
+                		int redB = (rgbB >> 16) & 0xff; 
+                		int greenB = (rgbB >> 8) & 0xff; 
+                		int blueB = (rgbB) & 0xff; 
+                		difference += Math.abs(redA - redB); 
+                		difference += Math.abs(greenA - greenB); 
+                		difference += Math.abs(blueA - blueB); 
+            		} 
+        	} 
 
-        // Total number of red pixels = width * height 
-        // Total number of blue pixels = width * height 
-        // Total number of green pixels = width * height 
-        // So total number of pixels = width * height * 3 
-        double total_pixels = width1 * height1 * 3; 
+        	// Total number of red pixels = width * height 
+        	// Total number of blue pixels = width * height 
+        	// Total number of green pixels = width * height 
+        	// So total number of pixels = width * height * 3 
+        	double total_pixels = width1 * height1 * 3; 
  
-        // Normalizing the value of different pixels 
-        // for accuracy(average pixels per color 
-        // component) 
-        double avg_different_pixels = difference / 
-                                      total_pixels; 
+        	// Normalizing the value of different pixels 
+        	// for accuracy(average pixels per color 
+        	// component) 
+        	double avg_different_pixels = difference / total_pixels; 
 
-        // There are 255 values of pixels in total 
-         percentage = (avg_different_pixels / 
-                                        255) * 100; 
+         	// There are 255 values of pixels in total 
+         	percentage = (avg_different_pixels / 255) * 100; 
 
         
-        //Lastly Detect if Enough Pixels are the Same
-        System.out.println("Photo Difference Percent " +  percentage); 
-        if(percentage <= 21.8) {
-            System.out.println("Face Detected");
-        } else {
-            System.out.println("Face Not Detected");
-        }
-        
-        
-    } 
+        	//Lastly Detect if Enough Pixels are the Same
+        	System.out.println("Photo Difference Percent " +  percentage); 
+        	if(percentage <= 21.8) {
+            		System.out.println("Face Detected");
+        	} else {
+           		System.out.println("Face Not Detected");
+        	}
+		
+    	} 
 } 
 
 
